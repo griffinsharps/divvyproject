7a155b6d0e1e8b39f6bc0803799d244e001ca675
griffinsharps <89551628+griffinsharps@users.noreply.github.com>
2022-10-02 15:12:18 -0500

    Cleaned up process data script

--- process_data.py
+++ process_data.py
@@ -1,28 +1,25 @@
 import json
 import requests
+
 url = "https://gbfs.divvybikes.com/gbfs/en/station_status.json"
 response = requests.get(url)
 stationtext = response.text
 stations = json.loads(stationtext)
 data = stations["data"]
 stationslist = data["stations"]
-sum = 0
+bikes = 0
+scooters = 0
+docks = 0
 for station in stationslist:
     q = station["num_bikes_available"]
-    sum = q + sum
-
-print("The Number of Divvy Bikes Currently Available in Chicago: " + str(sum))
-
-sum = 0
-for station in stationslist:
     r = station.get("num_scooters_available", 0)
-    sum = r + sum
-
-print("The Number of Scooters Currently Available in Chicago: " + str(sum))
-
-sum = 0
-for station in stationslist:
     s = station["num_docks_available"]
-    sum = s + sum
+    bikes = q + bikes
+    scooters = r + scooters
+    docks = s + docks
 
-print("The Number of Docks Currently Available in Chicago: " + str(sum))
+print("The Number of Divvy Bikes Currently Available in Chicago: " + str(bikes))
+
+print("The Number of Scooters Currently Available in Chicago: " + str(scooters))
+
+print("The Number of Docks Currently Available in Chicago: " + str(docks))
